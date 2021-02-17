1a7cf3dfbbdb5dfdbfc2f9bead32ec50b1084b35
Anufriev_on <Anufriev_on@mail.ru>
2021-02-17 20:02:47 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main (String[] args) {
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < 200000; i++) {
+            sb.append("ABC");
+        }
+
+        System.out.println((System.currentTimeMillis() - startTime));
+        System.out.println((System.currentTimeMillis() - startTime));
+    }
+}
