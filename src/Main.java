90c90416f6edd809f88f7610cb0e0b9ff97224d6
Anufriev_on <Anufriev_on@mail.ru>
2021-02-20 13:30:05 +0700

    branch2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 200000; i++) {
-            sb.append("ABC ABC2 bug fix");
+            sb.append("ABC ABC2 bug fix branch2");
         }
 
         System.out.println((System.currentTimeMillis() - startTime));
